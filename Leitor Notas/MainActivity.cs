611158edd487be5413856b5b5b5086815e9779f6
Altamir Benkenstein <Altamir Benkenstein>
2017-10-21 18:38:56 -0200

    Add project files.

---
+++ Leitor Notas/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Leitor_Notas
+{
+    [Activity(Label = "Leitor_Notas", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
